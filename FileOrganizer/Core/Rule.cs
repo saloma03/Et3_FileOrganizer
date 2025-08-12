742c25ab0fb299a0f63eb7426e206257b171b300
salma <salmaom3r@gmail.com>
2025-08-13 00:49:17 +0200

    feat: add SettingManager class and Rule Class

---
+++ FileOrganizer/Core/Rule.cs
@@ -0,0 +1,9 @@
+﻿namespace FileOrganizer.Core
+{
+    public class Rule
+    {
+        public string FolderName { get; set; } = default!;
+        public string Extension { get; set; } = default!;
+
+    }
+}
